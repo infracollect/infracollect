2eab0ca236b540a813dc4ee17db8f6262b3a1511
Adrien Fillon <adrien.fillon@gmail.com>
2026-01-25 13:39:44 +0100

    Missing assets
    Signed-off-by: Adrien Fillon <adrien.fillon@gmail.com>

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://infracollect.github.io",
+	base: "/infracollect",
 	integrations: [
 		starlight({
 			title: 'infracollect',
